3e96da6b1a2209a60cf34c164b9a4eff183b6dab
sayedosama7 <sayedosama088@gmail.com>
2024-05-29 00:56:27 +0300

    user login model

--- app/Model/userlogin.php
+++ app/Model/userlogin.php
@@ -3,10 +3,14 @@
 
 namespace App\Model;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Userlogin extends Model
+class Userlogin extends Model implements Authenticatable, JWTSubject
 {
+    use AuthenticableTrait;
     protected $fillable = [
         'id',
         'username',
@@ -17,7 +21,13 @@ class Userlogin extends Model
         'role',
         'created_by',
     ];
-    
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
-
-
